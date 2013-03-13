c82652c4b8f69ffdcaaaeabd1e3286ab1b37a4e5
subin basukala <subin.basukala@gmail.com>
2013-03-12 23:37:27 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+
+  
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type"><title>KhasiBazar</title>
+  
+  <style type="text/css">
+<!--
+.style1 {color: #FFFFFF}
+.style2 {font-family: Arial, Helvetica, sans-serif}
+-->
+  </style>
+  
+  <link rel="stylesheet" href="kb.css" type="text/css" />
+</head><body bgcolor="#89B5AC">
+<div align="center">
+  <table style="text-align: left; width: 80%; height: 477px; margin-left: auto; margin-right: auto;" border="0" cellpadding="0" cellspacing="0">
+    
+    <tbody>
+      <tr>
+        <td style="text-align: right; vertical-align: top; height: 26px;">
+        <span style="text-align: right;"><a href="login.php"><img src="Images/login.png" width="97" height="42" alt="Login"></a></span>
+        <span style="text-align: right;"><a href="signup.php"><img src="Images/signup.png" width="97" height="42" alt="Login"></a></span></td>
+      </tr>
+      <tr>
+        <td style="vertical-align: top; height: 385px;"><br>
+          <br>
+          <br>
+  <br>
+          <div style="text-align: center;"><img style="width: 321px; height: 236px;" alt="" src="Images/Logo.jpg"> <br>
+          </div>
+  </td>
+      </tr>
+      <tr>
+        <td style="text-align: center; vertical-align: top; height: 64px;"><input name="Search_box" type="text" size= "30" id="Search_box" value="Search">
+          <br>
+          <form name="form1" method="post" action="">&nbsp;&nbsp;
+              <input name="Search" type="submit" id="Search" value="Search">
+          </form>          
+          <br></td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <br>
+  
+  <br>
+</div>
+
+</body></html>
